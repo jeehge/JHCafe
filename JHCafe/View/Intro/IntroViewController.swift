99c1d2b6d4001e1969f717148f270c69aa0ee29c
jeehge <taerg89@gmail.com>
2022-10-23 18:56:14 +0900

    Add Intro View

---
+++ JHCafe/View/Intro/IntroViewController.swift
@@ -0,0 +1,16 @@
+//
+//  IntroViewController.swift
+//  JHCafe
+//
+//  Created by JH on 2022/10/23.
+//
+
+import UIKit
+
+final class IntroViewController: BaseViewController {
+	
+	// MARK: - Life Cycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
