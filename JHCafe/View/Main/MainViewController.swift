4d61200e0de887b307809dc2c50ec86dc8bf4028
jeehge <taerg89@gmail.com>
2022-10-25 23:04:01 +0900

    Add Main View

---
+++ JHCafe/View/Main/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  JHCafe
+//
+//  Created by JH on 2022/10/25.
+//
+
+import UIKit
+
+final class MainViewController: BaseViewController {
+	
+	// MARK: - Life Cycle
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
