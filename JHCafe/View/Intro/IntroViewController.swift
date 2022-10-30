2eb23048be3088285887a2ecf8f3bf19fdd771cb
jeehge <taerg89@gmail.com>
2022-10-30 21:53:40 +0900

    Set Intro View

--- JHCafe/View/Intro/IntroViewController.swift
+++ JHCafe/View/Intro/IntroViewController.swift
@@ -5,32 +5,32 @@
 //  Created by JH on 2022/10/23.
 //
 
-import Foundation
 import Lottie
+import UIKit
 
 final class IntroViewController: BaseViewController {
     // MARK: - Life Cycle
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        initIntroAnimation()
-    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		initIntroAnimation()
+	}
 
     // MARK: - initialize
 
     private func initIntroAnimation() {
-        let animationView: LottieAnimationView = .init(name: "39177-coffee-beans")
+		let animationView: LottieAnimationView = .init(name: "39177-coffee-beans")
         view.addSubview(animationView)
 
-        animationView.frame = view.bounds
+		animationView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         animationView.center = view.center
         animationView.contentMode = .scaleAspectFit
 
-        animationView.loopMode = .playOnce
+		animationView.loopMode = .autoReverse
         animationView.play()
 
-        Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false, block: { _ in
+        Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false, block: { _ in
             self.nextViewController()
         })
     }
